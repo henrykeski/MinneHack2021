6f0460830cd4b7c816e01e199f7c939aacd39871
Henry Keskitalo <keski027@d.umn.edu>
2021-01-23 17:13:21 -0600

    added graph and basic outline for AnalyticsActivity.

--- RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
+++ RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.;
+
 public class AnalyticsActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +16,17 @@ public class AnalyticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
     }
+
+    GraphView graph = (GraphView) findViewById(R.id.graph);
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+            new DataPoint(0, 1),
+            new DataPoint(1, 5),
+            new DataPoint(2, 3),
+            new DataPoint(3, 2),
+            new DataPoint(4, 6)
+    });
+//graph.addSeries(series);
+
+
+
 }
