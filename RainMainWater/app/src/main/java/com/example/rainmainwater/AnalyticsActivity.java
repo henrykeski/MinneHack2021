81c1ddf68408e540f27af2446cb8469ed0264131
Henry Keskitalo <keski027@d.umn.edu>
2021-01-23 18:19:16 -0600

    Analytic activity graph addition

--- RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
+++ RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.;
 
 public class AnalyticsActivity extends AppCompatActivity {
 
@@ -15,17 +14,18 @@ public class AnalyticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
-    GraphView graph = (GraphView) findViewById(R.id.graph);
-    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-            new DataPoint(0, 1),
-            new DataPoint(1, 5),
-            new DataPoint(2, 3),
-            new DataPoint(3, 2),
-            new DataPoint(4, 6)
-    });
-//graph.addSeries(series);
+
 
 
 
