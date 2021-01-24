373b2c36e5b6a01980e9d81cbc6a48fa6890f157
= <=>
2021-01-24 10:54:39 -0600

    Spruced up graph views and analytics layout

--- RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
+++ RainMainWater/app/src/main/java/com/example/rainmainwater/AnalyticsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,10 +13,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -29,6 +32,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import static java.lang.Math.round;
+
 public class AnalyticsActivity extends AppCompatActivity {
 
     ProgressBar PWaterLevel;
@@ -38,6 +43,13 @@ public class AnalyticsActivity extends AppCompatActivity {
 
     int array[];
     GraphView graph;
+    GraphView graph2;
+    GraphView graph3;
+
+    FloatingActionButton refreshButton;
+
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
 
     @Override
@@ -45,11 +57,74 @@ public class AnalyticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analytics);
         graph = (GraphView) findViewById(R.id.graph);
-        waterUsage = findViewById(R.id.WaterUsage);
+        graph2 = (GraphView) findViewById(R.id.graph2);
+        graph3 = (GraphView) findViewById(R.id.graph3);
+        refreshButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+//        waterUsage = findViewById(R.id.WaterUsage);
         waterLevel = findViewById(R.id.WaterLevel);
-        waterRatio = findViewById(R.id.WaterRatio);
+//        waterRatio = findViewById(R.id.WaterRatio);
         PWaterLevel = findViewById(R.id.progressBar);
         PWaterLevel.setMax(100);
+
+        refresh(refreshButton);
+
+    }
+
+
+    private void makeGraph(JSONObject response) throws JSONException {
+   //     parseString(response.getString("points"));  //Parses the getStrings of the data
+        JSONArray c = response.getJSONArray("pairs");
+        SimpleDateFormat formatter6=new SimpleDateFormat("MM-dd-yyyy");
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{});
+        for (int i = 0 ; i < c.length(); i++) {
+            JSONObject obj = c.getJSONObject(i);
+            double level = Double.parseDouble(df.format(obj.getDouble("dailyAvgLevel")));
+            String date = "";
+
+
+            try {
+                  date = obj.getString("_id") + "-2021";
+//                  date = formatter6.parse(obj.getString("date").split("T")[0]);
+                  System.out.println(date);
+            }
+            catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Having a hard time parsing in makeGraph... here's the raw _id: " + obj.getString("_id"));
+
+            }
+            series.appendData(new DataPoint(i, level), false, 31);
+
+        }
+
+        graph.addSeries(series);
+        graph.setTitle("Tank water level vs. elapsed days");
+    }
+    private void makeMainsConsumptGraph(JSONObject response) throws JSONException {
+        //     parseString(response.getString("points"));  //Parses the getStrings of the data
+        JSONArray c = response.getJSONArray("pairs");
+        SimpleDateFormat formatter6=new SimpleDateFormat("MM-dd-yyyy");
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{});
+        for (int i = 0 ; i < c.length(); i++) {
+            JSONObject obj = c.getJSONObject(i);
+            double level = Double.parseDouble(df.format(obj.getDouble("dailyVolume")));
+            Date date = new Date();
+            try {
+                date = formatter6.parse(obj.getString("_id")+ "-2021");
+                System.out.println(obj.getString("_id"));
+            }
+            catch (ParseException e) {
+                System.out.println("Having a hard time parsing in makeMainsConsumpt... here's the raw date: " + obj.getString("_id"));
+                System.out.println(e);
+            }
+            series.appendData(new DataPoint(i, level), false, 31);
+
+        }
+
+        graph2.setTitle("City water consumed (liters) vs. elapsed days");
+        graph2.addSeries(series);
+    }
+
+    public void refresh(View view){
         JSONObject newEventData = new JSONObject();
         Date today = new Date();
         Calendar cal = new GregorianCalendar();
@@ -161,62 +236,10 @@ public class AnalyticsActivity extends AppCompatActivity {
 
  */
 
-        waterUsage = findViewById(R.id.WaterUsage);
+//        waterUsage = findViewById(R.id.WaterUsage);
         waterLevel = findViewById(R.id.WaterLevel);
-        waterRatio = findViewById(R.id.WaterRatio);
+//        waterRatio = findViewById(R.id.WaterRatio);
 
-    }
-
-
-    private void makeGraph(JSONObject response) throws JSONException {
-   //     parseString(response.getString("points"));  //Parses the getStrings of the data
-        JSONArray c = response.getJSONArray("pairs");
-        SimpleDateFormat formatter6=new SimpleDateFormat("MM-dd-yyyy");
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{});
-        for (int i = 0 ; i < c.length(); i++) {
-            JSONObject obj = c.getJSONObject(i);
-            double level = obj.getDouble("dailyLevel");
-            Date date = new Date();
-
-
-            try {
-                  date = formatter6.parse(obj.getString("_id") + "-2021");
-//                  date = formatter6.parse(obj.getString("date").split("T")[0]);
-                  System.out.println(date);
-            }
-            catch (Exception e) {
-                System.out.println(e);
-                System.out.println("Having a hard time parsing in makeGraph... here's the raw _id: " + obj.getString("_id"));
-
-            }
-            series.appendData(new DataPoint(date, level), false, 100);
-
-        }
-
-        graph.addSeries(series);
-    }
-    private void makeMainsConsumptGraph(JSONObject response) throws JSONException {
-        //     parseString(response.getString("points"));  //Parses the getStrings of the data
-        JSONArray c = response.getJSONArray("pairs");
-        SimpleDateFormat formatter6=new SimpleDateFormat("MM-dd-yyyy");
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{});
-        for (int i = 0 ; i < c.length(); i++) {
-            JSONObject obj = c.getJSONObject(i);
-            double level = obj.getDouble("dailyVolume");
-            Date date = new Date();
-            try {
-                date = formatter6.parse(obj.getString("_id")+ "-2021");
-                System.out.println(obj.getString("_id"));
-            }
-            catch (ParseException e) {
-                System.out.println("Having a hard time parsing in makeMainsConsumpt... here's the raw date: " + obj.getString("_id"));
-                System.out.println(e);
-            }
-            series.appendData(new DataPoint(date, level), false, 100);
-
-        }
-
-//        graph.addSeries(series); TODO make a new graph object to add this to!
     }
 
     private void updateWaterUsage(JSONObject res) throws JSONException {
@@ -224,7 +247,7 @@ public class AnalyticsActivity extends AppCompatActivity {
     }
     private void updateWaterLevel(JSONObject res) throws JSONException {
         double level = res.getDouble("result");
-        waterLevel.setText(Double.toString(level/4095*100));
+        waterLevel.setText(Double.toString(Double.parseDouble(df.format(level/4095*100))));
         PWaterLevel.setProgress((int) (level/4095*100));
     }
 
